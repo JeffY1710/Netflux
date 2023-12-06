dc6cc89bfd7416fe781c60998fd08c440aa9de39
JeffY1710 <jeff.yabas@gmail.com>
2023-12-06 09:04:30 +0100

    reformat root page

--- src/pages/Root.tsx
+++ src/pages/Root.tsx
@@ -3,16 +3,10 @@ import { Header } from "../components/common/Header";
 
 export default function Root(){
     return(
-        <html lang="en">
-        <head>
-            <meta charSet="UTF-8" />
-            <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-            <title>Document</title>
-        </head>
-        <body>
+        <>
             <Header />
             <Outlet />
-        </body>
-        </html>
+        </>
+          
     )
 }
