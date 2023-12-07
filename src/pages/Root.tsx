55af8d4f715fad10e1c8f353ad17ac039c832058
lucas <lucasturschwell@hotmail.com>
2023-12-07 22:32:17 +0100

    ajout shows API to Frontend et details page

--- src/pages/Root.tsx
+++ src/pages/Root.tsx
@@ -1,6 +1,7 @@
 import { Outlet } from "react-router-dom";
 import { test } from "../data/firebase/global";
 import Header from "../components/common/Header";
+import AfficherCardHome from "../components/liste_series/AfficherCardHome";
 //import { currentUser } from "../data/firebase/global";
 
 export default function Root(){
@@ -8,6 +9,7 @@ export default function Root(){
         <>
             <Header />
             <Outlet />
+            <AfficherCardHome/>
         </>
           
     )
